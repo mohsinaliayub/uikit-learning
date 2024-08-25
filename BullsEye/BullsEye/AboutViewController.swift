39cb35add7368a85b00fe556cd16f260dd2dd55d
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-25 02:24:37 +0200

    add an about screen

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mohsin Ali Ayub on 25.08.24.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true)
+    }
+    
+}
