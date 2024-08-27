5d84b124e5024d376100d13a8b59238eaae4434f
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-27 23:16:30 +0200

    disable done button if text field is empty

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK: - Actions
@@ -33,5 +37,24 @@ class AddItemViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+}
+
+extension AddItemViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // get old text -> replace old text (in a range) with new text
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        // disable the done button if text field is empty
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
