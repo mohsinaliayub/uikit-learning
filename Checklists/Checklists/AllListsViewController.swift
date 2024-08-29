e9bab0d005b36e9c2efd32a18ce0a02f76e7c412
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-30 00:27:44 +0200

    add all lists view controller

---
+++ Checklists/Checklists/AllListsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Mohsin Ali Ayub on 30.08.24.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    
+    private let cellIdentifier = "ChecklistCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "List \(indexPath.row)"
+        
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+}
