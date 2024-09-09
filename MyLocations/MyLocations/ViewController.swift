8db1bdb631c3cd0ebdc6c4342558700eacb40c14
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-09-10 01:50:53 +0200

    create MyLocations project

---
+++ MyLocations/MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Mohsin Ali Ayub on 10.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
