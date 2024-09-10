21f2020d8a278607d60f86953021c2b2882d3bbb
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-09-10 02:38:58 +0200

    add a tab view controller

--- MyLocations/MyLocations/View Controllers/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/View Controllers/CurrentLocationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrentLocationViewController.swift
 //  MyLocations
 //
 //  Created by Mohsin Ali Ayub on 10.09.24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrentLocationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
